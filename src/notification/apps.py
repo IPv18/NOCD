dbdf0e48593cccc247757f7cab9834d323c23b76
dori <dorifeet@gmail.com>
2023-04-14 15:42:35 +0300

    implemented the notification app

--- src/notification/apps.py
+++ src/notification/apps.py
@@ -1,6 +1,28 @@
+from os import environ
 from django.apps import AppConfig
+from notification import notification as notif
 
 
 class NotificationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'notification'
+
+    def ready(self):
+        from notification import jobs
+        if environ.get('RUN_MAIN', None) != 'true':
+            self.add_notifications()
+            if environ.get('DEBUG') == True:
+                jobs.start_scheduler()
+
+    def add_notifications(self):
+        notif.add_notification('info', type='info', message='info message')
+        notif.add_notification('warning', type='warning',
+                               message='warning message')
+
+        def send_notification(type, message):
+            from notification.models import NotificationInfo
+            notification_info = NotificationInfo(type=type, message=message)
+            notification_info.save()
+
+        notif.subscribe('info', send_notification)
+        notif.subscribe('warning', send_notification)
--- src/notification/views.py
+++ src/notification/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_POST
+from django.core.paginator import Paginator
+from django.contrib import messages
+from django.utils import timezone
+from django.http import Http404
 
-# Create your views here.
+from notification.models import NotificationInfo
+
+
+def notifications(request):
+    notifications = NotificationInfo.objects.order_by('-date')
+
+    paginator = Paginator(notifications, 10)
+    page_number = request.GET.get('page')
+
+    try:
+        page_obj = paginator.get_page(page_number)
+    except:
+        raise Http404("Invalid page number")
+
+    context = {'page_obj': page_obj}
+    return render(request, 'notification/notifications.html', context)
+
+
+def add_notification(request):
+    if request.method == 'POST':
+        message = request.POST['message']
+        type = request.POST['type']
+        date = timezone.now()
+        notification = NotificationInfo(message=message, type=type, date=date)
+        notification.save()
+        messages.success(request, 'Notification added.')
+        return redirect('notifications')
+
+
+def delete_notification(request, id):
+    notification = NotificationInfo.objects.get(id=id)
+    notification.delete()
+    messages.success(request, 'Notification deleted.')
+    return redirect('notifications')
+
+@require_POST
+def clear_notifications(request):
+    NotificationInfo.objects.all().delete()
+    messages.success(request, 'All notifications cleared.')
+    return redirect('notifications')
+
+def toggle_read_notification(request, id):
+    notification = NotificationInfo.objects.get(id=id)
+    notification.read = not notification.read
+    notification.save()
+    messages.success(request, 'Notification read toggled.')
+    return redirect('notifications')
