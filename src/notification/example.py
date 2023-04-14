0d067563fb577979d1b9fd00c0ace3299f7cd862
dori <dorifeet@gmail.com>
2023-04-14 22:39:17 +0300

    corrected script to use new names

--- src/notification/example.py
+++ src/notification/example.py
@@ -1,4 +1,4 @@
-import notification as notif
+from notification import topic
 from time import sleep
 
 
@@ -7,23 +7,23 @@ def print_arguments(a):
     print(a)
 
 
-# Create a notification
-notif.add_notification('Test', a=0)
+# Create a topic
+topic.add_topic('Test', a=0)
 
-# Subscribe to the Test notification
-notif.subscribe('Test', print_arguments)
-
-# Simulate code running
-sleep(1.0)
-
-# Send notification
-notif.send('Test')
-
-# Change the notification arguments
-notif.change_notification_args('Test', a=1)
+# Subscribe to the Test topic
+topic.subscribe('Test', print_arguments)
 
 # Simulate code running
 sleep(1.0)
 
-# Send notification again
-notif.send('Test')
+# Send topic
+topic.send('Test')
+
+# Change the topic arguments
+topic.change_topic_args('Test', a=1)
+
+# Simulate code running
+sleep(1.0)
+
+# Send topic again
+topic.send('Test')
