be26b163f889044a2169f46b6683813350a2edd7
OsamaMomani <osamaalmomani32@gmail.com>
2023-04-16 03:20:54 +0300

    Add error handling
    For cases where the table isn't migrated yet

--- src/firewall/apps.py
+++ src/firewall/apps.py
@@ -20,19 +20,24 @@ class FirewallConfig(AppConfig):
             FirewallRule.objects.filter(
                 ip_family='IPv6', traffic_direction='Outbound', rule_id=1000),
         ]
+        
+        try:
+            if not rules[0]:
+                FirewallRule.objects.create(
+                    ip_family='IPv4', traffic_direction='Inbound', rule_id=1000, description="ALL TRAFFIC", action='ACCEPT')
 
-        if not rules[0]:
-            FirewallRule.objects.create(
-                ip_family='IPv4', traffic_direction='Inbound', rule_id=1000, description="ALL TRAFFIC", action='ACCEPT')
+            if not rules[1]:
+                FirewallRule.objects.create(
+                    ip_family='IPv4', traffic_direction='Outbound', rule_id=1000, description="ALL TRAFFIC", action='ACCEPT')
 
-        if not rules[1]:
-            FirewallRule.objects.create(
-                ip_family='IPv4', traffic_direction='Outbound', rule_id=1000, description="ALL TRAFFIC", action='ACCEPT')
+            if not rules[2]:
+                FirewallRule.objects.create(
+                    ip_family='IPv6', traffic_direction='Inbound', rule_id=1000, description="ALL TRAFFIC", action='ACCEPT')
 
-        if not rules[2]:
-            FirewallRule.objects.create(
-                ip_family='IPv6', traffic_direction='Inbound', rule_id=1000, description="ALL TRAFFIC", action='ACCEPT')
-
-        if not rules[3]:
-            FirewallRule.objects.create(
-                ip_family='IPv6', traffic_direction='Outbound', rule_id=1000, description="ALL TRAFFIC", action='ACCEPT')
+            if not rules[3]:
+                FirewallRule.objects.create(
+                    ip_family='IPv6', traffic_direction='Outbound', rule_id=1000, description="ALL TRAFFIC", action='ACCEPT')
+        except Exception as e:
+            # TODO: log error or handle exception
+            pass
+        
