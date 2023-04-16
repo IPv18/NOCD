66d249e4a62cf1f51f210b0ae7ded648c2159527
osama abu oshaibah <osamahusam295@gmail.com>
2023-04-16 04:39:34 -0400

    fixed some errors

--- src/firewall/apps.py
+++ src/firewall/apps.py
@@ -20,22 +20,6 @@ class FirewallConfig(AppConfig):
                 ip_family='IPv6', traffic_direction='Outbound', rule_priority=1000),
         ]
 
-        if not rules[0]:
-            FirewallRule.objects.create(
-                ip_family='IPv4', traffic_direction='Inbound', rule_priority=1000, description="ALL TRAFFIC", action='ACCEPT')
-
-        if not rules[1]:
-            FirewallRule.objects.create(
-                ip_family='IPv4', traffic_direction='Outbound', rule_priority=1000, description="ALL TRAFFIC", action='ACCEPT')
-
-        if not rules[2]:
-            FirewallRule.objects.create(
-                ip_family='IPv6', traffic_direction='Inbound', rule_priority=1000, description="ALL TRAFFIC", action='ACCEPT')
-
-        if not rules[3]:
-            FirewallRule.objects.create(
-                ip_family='IPv6', traffic_direction='Outbound', rule_priority=1000, description="ALL TRAFFIC", action='ACCEPT')
-        
         try:
             if not rules[0]:
                 FirewallRule.objects.create(
