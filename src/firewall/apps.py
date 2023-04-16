a1530a53bb4f24f8426002155509e1348d2f148c
osama abu oshaibah <osamahusam295@gmail.com>
2023-04-16 08:22:03 -0400

    fixed more errors and branch is ready to be merged

--- src/firewall/apps.py
+++ src/firewall/apps.py
@@ -14,13 +14,17 @@ class FirewallConfig(AppConfig):
         try:
             if not rules:
                 FirewallRule.objects.create(
-                    ip_family='IPv4', traffic_direction='Inbound', rule_priority=1000, description="ALL TRAFFIC", action='ACCEPT')
+                    ip_family='IPv4', traffic_direction='Inbound', rule_priority=1000, 
+                    description="ALL TRAFFIC", type="", protocol="", action='ACCEPT')
                 FirewallRule.objects.create(
-                    ip_family='IPv4', traffic_direction='Outbound', rule_priority=1000, description="ALL TRAFFIC", action='ACCEPT')
+                    ip_family='IPv4', traffic_direction='Outbound', rule_priority=1000,
+                     description="ALL TRAFFIC", type="", protocol="", action='ACCEPT')
                 FirewallRule.objects.create(
-                    ip_family='IPv6', traffic_direction='Inbound', rule_priority=1000, description="ALL TRAFFIC", action='ACCEPT')
+                    ip_family='IPv6', traffic_direction='Inbound', rule_priority=1000,
+                     description="ALL TRAFFIC", type="", protocol="", action='ACCEPT')
                 FirewallRule.objects.create(
-                    ip_family='IPv6', traffic_direction='Outbound', rule_priority=1000, description="ALL TRAFFIC", action='ACCEPT')
+                    ip_family='IPv6', traffic_direction='Outbound', rule_priority=1000,
+                     description="ALL TRAFFIC", type="", protocol="", action='ACCEPT')
         except Exception as e:
             # TODO: log error or handle exception
             pass
