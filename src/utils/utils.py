8dbd09a5d1f380ad52dd7d5bcacb77e0f5ae8972
OsamaMomani <osamaalmomani32@gmail.com>
2023-04-17 06:26:52 +0300

    Add utils module docstring

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,3 +1,16 @@
+"""
+The `scapy_helper` module provides helper functions for working with the Scapy network tool.
+
+Functions:
+- `get_local_macs()`: Returns a list of the local MAC addresses.
+- `get_interfaces()`: Returns a list of the local network interfaces, 
+                      excluding the loopback interface.
+
+Dependencies:
+- `scapy.all`: The Scapy library, which must be installed in order to use this module.
+"""
+
+
 from scapy.all import conf
 
 def get_local_macs():
