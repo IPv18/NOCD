2d14a98f2423f7d1ccb465a1e8de6b5c7867f396
OsamaMomani <osamaalmomani32@gmail.com>
2023-04-18 12:49:17 +0300

    Major refactor and fixes
    - Split SocketInfo into IPLink, PacketRecord, and SocketRecord
    for better polymorphism and convenience
    - Implement dataclass
    - Fix issues with socket buffer, like ordering.

--- src/network_sniffer/utils/ss_helper.py
+++ src/network_sniffer/utils/ss_helper.py
@@ -1,9 +1,9 @@
 """
 A module for working with network sockets on Linux using the iproute2/ss command.
 
-The module provides two classes: SocketInfo, and SsHelper.
+The module provides two classes: PacketRecord, and SsHelper.
 
-SocketInfo: 
+PacketRecord: 
 A class that represents information for a network socket connection
 between two hosts and ports on the network.
 The class has the following attributes:
@@ -23,7 +23,7 @@ it has PacketRingBuf class and object (sockets_buf):
 A ring buffer that stores the last n packet info in a FIFO manner
 
 It has the following methods:
-    - parse_ss_output: Parses the output of the ss command and returns a SocketInfo object.
+    - parse_ss_output: Parses the output of the ss command and returns a PacketRecord object.
     - update_buffer: Runs the ss command and updates sockets in buffer or 
       creates a new one if it doesn't exist.
     - update_socket_info: Updates the socket info with the latest info from the system.
@@ -31,19 +31,19 @@ It has the following methods:
 Note: The SsHelper methods may require sudo privileges.
 
 Usage:
-To use this module, you can import it and create an instance of the SocketInfo class.
+To use this module, you can import it and create an instance of the PacketRecord class.
 You can then use the various methods provided by the SsHelper class
 to update the socket info object with the latest information from the system.
 
 Example:
 
-from ss_helper import SocketInfo, SsHelper
+from ss_helper import PacketRecord, SsHelper
 
 # update the sockets buffer
 SsHelper.update_buffer()
 
 # create a socket info object
-socket_info = SocketInfo(ip_src="127.0.0.1", port_src=8080, protocol="tcp")
+socket_info = PacketRecord(ip_src="127.0.0.1", port_src=8080, protocol="tcp")
 
 # update the socket info object with the latest information from the system
 socket_info.update_info()
@@ -60,75 +60,64 @@ import subprocess
 
 from collections import OrderedDict
 from dataclasses import dataclass
+from utils.utils import get_local_macs
+
+LOCAL_MAC_LIST = get_local_macs()
 
 
 @dataclass(slots=True)
-class SocketInfo:
+class IPLink:
     '''
-    A class that represents information for a network socket
-    connection between two hosts and ports on the network
+    A data class that represents information for a IP network link 
+    (regardless connection or connectionless) between two hosts and ports on the network
     '''
 
+    protocol: str = None
     ip_src: str = None
     port_src: int = None
     ip_dest: str = None
     port_dest: int = None
-    program: str = None
-    protocol: str = None
+
+
+@dataclass(slots=True)
+class PacketRecord(IPLink):
+    '''
+    A class that represents information for a network traffic
+    record between two hosts and ports on the network
+    '''
+    timestamp: int = None
+    length: int = None
+    mac_src: str = None
+    mac_dest: str = None
     direction: str = None
+    program: str = None
 
     def __post_init__(self):
         self.protocol = self.protocol.lower() if self.protocol else None
+        self.update_info()
 
 
     def __iter__(self) -> iter:
         '''
-        Returns an iterator of key value pairs for all the attributes in the socket info objec.
+        Returns an iterator of key value pairs for all the attributes in the record.
         '''
 
         return iter({attr: getattr(self, attr) for attr in self.__slots__}.items())
 
 
-    def __eq__(self, other) -> bool:
-        '''
-        Compares two socket info objects.
-        Two socket info objects can be equal even if their ( ip_dest, port_dest, direction ) diffe.
-        '''
-
-        if isinstance(other, SocketInfo):
-            return all(
-                (getattr(self, attr) == getattr(other, attr)) \
-                    for attr in ("ip_src", "port_src", "protocol")
-            )
-        elif isinstance(other, tuple):
-            return all(
-                (getattr(self, attr) == other[i]) \
-                    for i, attr in enumerate(("ip_src", "port_src", "protocol"))
-            )
-        else:
-            return False
-
-
-    def __hash__(self) -> int:
-        '''
-        Returns a hash value for the socket info object.
-        Two socket info hashes can be equal even if their ( ip_dest, port_dest, direction ) diffe.
-        '''
-
-        return hash((self.ip_src, self.port_src, self.protocol))
-
-
     def __str__(self) -> str:
         '''
-        Returns a pretty string representation of the socket info objec.
+        Returns a pretty string representation of the record object.
         '''
 
         return \
             f"""
+            Timestamp: {self.timestamp}
             Program  : {self.program}
             Protocol : {self.protocol}
             Direction: {self.direction}
-            IP       : {self.ip_src}:{self.port_src} --> {self.ip_dest}:{self.port_dest}
+            IP       : { {self.ip_src}:{self.port_src}:<{20} } --> {self.ip_dest}:{self.port_dest}
+            MAC      : { self.mac_src:<{20} } --> {self.mac_dest}
             """
 
 
@@ -142,18 +131,59 @@ class SocketInfo:
 
     def update_info(self) -> None:
         '''
-        Updates the socket info with the latest info from the syste.
+        Updates the network traffic record with the latest info from the system.
         '''
-        SsHelper.update_socket_info(self)
+        SsHelper.update_packet_record(self)
 
 
     def copy(self, other) -> None:
         '''
-        Copies the attributes of the other socket info object to this socket info object.
+        Deep copies the attributes of the other record object to this record object.
         '''
 
-        for attr in self.__slots__:
+        for attr in other.__slots__:
             setattr(self, attr, getattr(other, attr))
+
+
+@dataclass(slots=True)
+class SocketRecord(IPLink):
+
+    program: str = None
+
+    def __eq__(self, other) -> bool:
+        '''
+        Compares two record objects.
+        Two record objects can be equal given their 
+        ("ip_src", "port_src", "protocol", "program") are equal.
+        '''
+        if isinstance(other, SocketRecord):
+            # if two records being inserted -or checked- and they have
+            # the same ("ip_src", "port_src", "protocol", "program") then
+            # this is the same socket and we should update the existing record
+            # i.e. Update instead of inserting a new one
+            return all(
+                (getattr(self, attr) == getattr(other, attr)) \
+                    for attr in ("ip_src", "port_src", "protocol", "program")
+            )
+        if isinstance(other, tuple):
+            # This case for indexing by a tuple TODO
+            # buffer[("ip_src", "port_src", "protocol")] instead of SocketRecord
+            # Used for retrieve or check if tuple exists in the buffer as a key
+            return all(
+                (getattr(self, attr) == other[i]) \
+                    for i, attr in enumerate(("ip_src", "port_src", "protocol"))
+            )
+        return False
+
+
+    def __hash__(self) -> int:
+        '''
+        Returns a hash value for the record object.
+        Two record hashes can be equal given their 
+        ("ip_src", "port_src", "protocol", "program") are equal.
+        '''
+
+        return hash((self.ip_src, self.port_src, self.protocol))
 
 
 class SsHelper():
@@ -172,6 +202,9 @@ class SsHelper():
             super().__init__(*args, **kwargs)
 
         def __setitem__(self, key, value) -> None:
+            # del pld item to maintain FIFO order based on update time
+            if key in self:
+                del self[key]
             OrderedDict.__setitem__(self, key, value)
             if self._size > 0:
                 if len(self) > self._size:
@@ -183,17 +216,17 @@ class SsHelper():
     @staticmethod
     def parse_ss_output(line: str)  -> None:
         '''
-        Parses the output of the ss command and returns a SocketInfo objec.
+        Parses the output of the ss command and returns a SocketRecord objec.
         '''
         cols = line.split(" ")
-        return SocketInfo(
+        return SocketRecord(
             ip_src=cols[4].rsplit(":", 1)[0].split("%")[0],
             port_src=cols[4].rsplit(":", 1)[1],
             ip_dest=cols[5].rsplit(":", 1)[0].split("%")[0],
             port_dest=cols[5].rsplit(":", 1)[1],
             protocol=cols[0].lower(),
             program=cols[6].split('"')[1]
-            if '"' in cols[6] else "Unknown"
+            if len(cols) > 6 and '"' in cols[6] else ""
         )
 
     @classmethod
@@ -215,24 +248,32 @@ class SsHelper():
                 line = line.decode("utf-8").strip()
                 new_socket = cls.parse_ss_output(line)
                 cls.sockets_buf[new_socket] = new_socket
-
         except Exception as ex:
             print(f"Error while updating ss buffer: {ex}")
             # TODO - add logger and log this
             # TODO - add exception handling
 
-
     @classmethod
-    def update_socket_info(cls, socket_info:SocketInfo)  -> bool:
+    def update_packet_record(cls, packet_record:PacketRecord)  -> None:
         '''
-        Returns the socket that has created the input packet.
+        Updates the PacketRecord with the SocketRecord info that has created it.
+        For now, we only update the dirction and program name.
         '''
 
-        ip_src = socket_info.ip_src
-        port_src = socket_info.port_src
-        ip_dest = socket_info.ip_dest
-        port_dest = socket_info.port_dest
-        protocol = socket_info.protocol
+        mac_src     = packet_record.mac_src
+        mac_dest    = packet_record.mac_dest
+        ip_src      = packet_record.ip_src
+        port_src    = packet_record.port_src
+        ip_dest     = packet_record.ip_dest
+        port_dest   = packet_record.port_dest
+        protocol    = packet_record.protocol
+
+        if mac_src in LOCAL_MAC_LIST: 
+            packet_record.direction = "outbound"
+        elif mac_dest in LOCAL_MAC_LIST:
+            packet_record.direction = "inbound"
+        else:
+            packet_record.direction = "captured(promiscuous?)"
 
         keys_to_check = [
             (ip_src, port_src, protocol),
@@ -248,18 +289,12 @@ class SsHelper():
             ("*", "*", protocol),
         ]
 
-        for key in keys_to_check:
-            if key in cls.sockets_buf.keys():
-                result = cls.sockets_buf[key]
-                if ip_src in key or port_src in key:
-                    result.direction = "outbound"
-                else:
-                    result.direction = "inbound"
-                # Deep copy the socket info object
-                socket_info.copy(result)
-                return True
+        buf = cls.sockets_buf
 
-        return False
+        for key in keys_to_check:
+            if key in buf.keys():
+                packet_record.program = buf[key].program # This can be dengerous if there are multiple sockets with the same key
+                return
 
 
 class SsHelperTest():
@@ -274,7 +309,7 @@ class SsHelperTest():
         '''
         cls.test_parse_ss_output()
         cls.test_update_buffer()
-        cls.test_update_socket_info()
+        cls.test_update_packet_info()
 
     @staticmethod
     def test_parse_ss_output()  -> None:
@@ -312,24 +347,20 @@ class SsHelperTest():
 
 
     @staticmethod
-    def test_update_socket_info() -> None:
+    def test_update_packet_info() -> None:
         '''
         Tests the update_socket_info method
         '''
         target_socket = next(iter(SsHelper.sockets_buf.keys()))
-        result = SocketInfo(
+        result = PacketRecord(
             ip_src=target_socket.ip_src,
             port_src=target_socket.port_src,
             protocol=target_socket.protocol
         )
-        SsHelper.update_socket_info(result)
+        SsHelper.update_packet_record(result)
         print(result)
         assert result is not None
         assert result == target_socket
-        assert result.ip_dest is not None \
-            and result.ip_dest == target_socket.ip_dest
-        assert result.port_dest is not None \
-            and result.port_dest == target_socket.port_dest
         assert result.program is not None \
             and result.program == target_socket.program
         assert result.direction is not None \
