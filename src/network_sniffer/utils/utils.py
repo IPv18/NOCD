8791b4140163989e7c00897b657b1d555f4daacc
OsamaMomani <osamaalmomani32@gmail.com>
2023-04-18 12:40:32 +0300

    Add get_local_ips and get_local_macs utils

--- src/network_sniffer/utils/utils.py
+++ src/network_sniffer/utils/utils.py
@@ -28,3 +28,41 @@ def get_interfaces():
     return [
         interface for interface in interfaces if interface and interface != "lo"
     ]
+
+
+def get_local_ips():
+    '''
+    Returns a list of the local network interfaces, excluding the loopback interface.
+    '''
+
+    output = subprocess.check_output(
+        ["""ip addr show | awk '/inet / {split($2,a,"/"); print a[1]}'"""],
+        shell=True
+    )
+
+    local_ips = output.decode("utf-8").split("\n")
+
+
+    return [
+        ip for ip in local_ips if ip
+    ]
+
+
+def get_local_macs():
+    '''
+    Returns a list of the local network interfaces, excluding the loopback interface.
+    '''
+
+    output = subprocess.check_output(
+        ["""ip addr show | awk '/ether / {split($2,a,"/"); print a[1]}'"""],
+        shell=True
+    )
+
+    local_ips = output.decode("utf-8").split("\n")
+
+
+    return [
+        ip for ip in local_ips if ip
+    ]
+
+
