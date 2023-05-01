5a9e11848a849726203602b1fc2f3cb44be9828d
osama abu oshaibah <osamahusam295@gmail.com>
2023-05-01 11:23:11 -0400

    .

--- src/firewall/urls.py
+++ src/firewall/urls.py
@@ -4,19 +4,23 @@ from . import views
 
 urlpatterns = [
     path('',                       views.index,         name="home"),
-    path('addrule/',               views.add_rule,      name="addrule"),    # TODO - this should be a POST request into index
-    path('updaterule/<str:pk>/',   views.update_rule,   name="updaterule"), # TODO - this should be a PUT request into rule_handler
-    path('removerule/<str:pk>/',   views.remove_rule,   name="removerule"), # TODO - this should be a DELETE request into rule_handler
+    path('addrule/',               views.add_rule,      name="addrule"),    
+    path('updaterule/<str:pk>/',   views.update_rule,   name="updaterule"), 
+    path('removerule/<str:pk>/',   views.remove_rule,   name="removerule"), 
     path('check_rule_uniqueness/', views.check_rule_uniqueness)
 ]
+
+
 # path('rule/<str:pk>/',views.rule_handler,   name="rule_handler")
+# TODO - addrule should be a POST request into index
+# TODO - update should be a PUT request into rule_handler
+# TODO - delete should be a DELETE request into rule_handler
+
 
 """
-
 urlpatterns = [
     path('',                   views.index_RESTful,         name="home"),
     path('rule/',              views.rule_RESTful ,         name="rule"),
     path('rule/<str:pk>/',     views.rule_handler_RESTFUL,  name="rule_handler")
 ]
-
 """
