cb13a63e5f24eb55d1b16c0e7570c34e0d3ca91f
OsamaMomani <osamaalmomani32@gmail.com>
2023-05-03 23:30:05 +0300

    Add ip-policy-form & modal

--- src/traffic_control/static/traffic_control/app.js
+++ src/traffic_control/static/traffic_control/app.js
@@ -4,30 +4,278 @@ var interface;
 var interfaces;
 
 
+function program_table_sort(){
+    // Workaround for header bug in DataTables
+    // where headers are scratched on page load
+    document.querySelector("#program-table_wrapper > div.row.dt-row > div > div > div.dataTables_scrollHead > div > table > thead > tr > th.sorting.sorting_asc").click();
+}
+
+function ip_table_sort(){
+    // Workaround for header bug in DataTables
+    // where headers are scratched on page load
+    document.querySelector("#ip-table_wrapper > div.row.dt-row > div > div > div.dataTables_scrollHead > div > table > thead > tr > th.sorting.sorting_asc").click();
+}
+
+
+function deleteProgramPolicy(btn) {
+    console.log("delete");
+    console.log(btn);
+    var deleteUrl = "/traffic_control/policy/" + btn.getAttribute("data-id") + "/";
+    $.ajax({
+        url: deleteUrl,
+        type: "DELETE",
+        success: function () {
+            // Remove the row from the table
+            $('#program-table').DataTable().ajax.reload()
+            console.log("Done deleting object!");
+        },
+        error: function () {
+            alert("Error deleting object!");
+        }
+    });
+}
+
+
 function renderProgramPolicy(data, type, row, meta) {
     col = meta.col;
     if (type === 'display') {
         console.log(row);
         switch (col) {
-            case 2:
-                return data.join(', ');
             case 11:
+                return `<button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#info-modal" data-bs-info='${JSON.stringify(row["description"])}'>
+                            <i class="fas fa-info-circle"></i>
+                        </button>
+            `;
+            case 10:
                 return data.slice(0, 16).replace('T', ' ');
 
             case 12:
-                return `<button class="btn btn-sm btn-primary">
+                return `<button type="button" class="btn btn-sm btn-primary"
+                            data-bs-toggle="modal"
+                            data-bs-target="#program-modal"
+                            data-bs-action="edit"
+                            data-bs-row='${JSON.stringify(row)}'>
                             <i class="fa fa-pencil"></i> 
-                        </button>`; 
+                        </button>`;
             case 13:
-                return `<button class=" btn btn-sm btn-danger delete-link " data-id="${row['id']} " >
+                return `<button class=" btn btn-sm btn-danger delete-link " data-id="${row['id']}" onclick="deleteProgramPolicy(this)">
                             <i class="fas fa-trash"></i> 
                         </button>`;
 
         }
     }
+
     return data;
 }
+
+
+
+function deleteIpPolicy(btn) {
+    console.log("delete");
+    console.log(btn);
+    var deleteUrl = "/traffic_control/policy/" + btn.getAttribute("data-id") + "/";
+    $.ajax({
+        url: deleteUrl,
+        type: "DELETE",
+        success: function () {
+            // Remove the row from the table
+            $('#ip-table').DataTable().ajax.reload()
+            console.log("Done deleting object!");
+        },
+        error: function () {
+            alert("Error deleting object!");
+        }
+    });
+}
+
+
+function renderIpPolicy(data, type, row, meta) {
+    col = meta.col;
+    if (type === 'display') {
+        console.log(row);
+        switch (col) {
+            case 15:
+                return `<button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal" data-bs-target="#info-modal" data-bs-info='${JSON.stringify(row["description"])}'>
+                            <i class="fas fa-info-circle"></i>
+                        </button>`;
+            case 14:
+                return data.slice(0, 16).replace('T', ' ');
+
+            case 16:
+                return `<button type="button" class="btn btn-sm btn-primary"
+                            data-bs-toggle="modal"
+                            data-bs-target="#ip-modal"
+                            data-bs-action="edit"
+                            data-bs-row='${JSON.stringify(row)}'>
+                            <i class="fa fa-pencil"></i> 
+                        </button>`;
+            case 17:
+                return `<button class=" btn btn-sm btn-danger delete-link " data-id="${row['id']}" onclick="deleteIpPolicy(this)">
+                            <i class="fas fa-trash"></i> 
+                        </button>`;
+
+        }
+    }
+
+    return data;
+}
+
+
 $(document).ready(function () {
+
+
+    // --------------------------------- Common ---------------------------------
+
+
+
+
+    const infoModal = document.getElementById('info-modal')
+    infoModal.addEventListener('show.bs.modal', event => {
+        // Button that triggered the modal
+        const button = event.relatedTarget
+        // Extract info from data-bs-* attributes
+        const info = button.getAttribute('data-bs-info')
+        // update the modal's content
+        const modalBody = infoModal.querySelector('.modal-body')
+        modalBody.textContent = info
+    })
+
+
+    // --------------------------------- Program  ---------------------------------
+    const programModal = document.getElementById('program-modal')
+    programModal.addEventListener('show.bs.modal', event => {
+        // Button that triggered the modal
+        const button = event.relatedTarget
+        if (button.getAttribute('data-bs-action') == 'edit') {
+            // Extract info from data-bs-* attributes
+            const row = JSON.parse(button.getAttribute('data-bs-row'))
+            // update the modal's content
+            $("#program-policy-name").val(row['name']);
+            $("#program-policy-programs").val(row['programs']);
+            $("#program-policy-rate").val(row['rate']);
+            $("#program-policy-burst").val(row['burst']);
+            $("#program-policy-prio").val(row['prio']);
+            $("#program-policy-description").val(row['description']);
+
+            $("#program-policy-direction option[value='" + row['direction'] + "']").prop('selected', true)
+            $("#modal-interface-selector option[value='" + row['interface'] + "']").prop('selected', true)
+            $("#program-policy-enabled").prop('checked', row['enabled']);
+            $("#program-policy-startup").prop('checked', row['startup']);
+            $("#program-policy-id").val(row['id']);
+
+
+            //const modalBodyInput = programModal.querySelector('.modal-body input')
+
+            
+            const modalTitle = programModal.querySelector('.modal-title')
+            //const modalBodyInput = programModal.querySelector('.modal-body input')
+            console.log(row);
+            modalTitle.textContent = `Edit program policy ID:${row['id']}`
+            //modalBodyInput.value = recipient
+        }
+    })
+
+
+    $("#program-policy-form").on('submit', function (e) {
+        e.preventDefault();
+        var form = $(this);
+        var url = form.attr('action');
+        var id = $("#program-policy-id").val();
+        var data = form.serialize();
+        console.log(data);
+        if (id != '') {
+            url = url + id + '/';
+            method = 'PUT';
+            console.log(url);
+        }
+        else {
+            method = 'POST';
+        }
+
+        $.ajax({
+            type: method,
+            url: url,
+            data: data,
+            success: function (data) {
+                console.log(data);
+                $('#program-table').DataTable().ajax.reload()
+                $('#program-modal').modal('hide');
+            },
+            error: function (err) {
+                console.log(err);
+                alert('Error: ' + err.responseText);
+            }
+        });
+    });
+
+
+
+    // --------------------------------- IP  ---------------------------------
+
+
+    const ipModal = document.getElementById('ip-modal')
+    ipModal.addEventListener('show.bs.modal', event => {
+        // Button that triggered the modal
+        const button = event.relatedTarget
+        if (button.getAttribute('data-bs-action') == 'edit') {
+            // Extract info from data-bs-* attributes
+            const row = JSON.parse(button.getAttribute('data-bs-row'))
+            // update the modal's content
+            $("#ip-policy-name").val(row['name']);
+
+            $("#ip-policy-rate").val(row['rate']);
+            $("#ip-policy-burst").val(row['burst']);
+            $("#ip-policy-prio").val(row['prio']);
+            $("#ip-policy-description").val(row['description']);
+
+            $("#ip-policy-direction option[value='" + row['direction'] + "']").prop('selected', true)
+            $("#modal-interface-selector option[value='" + row['interface'] + "']").prop('selected', true)
+            $("#ip-policy-enabled").prop('checked', row['enabled']);
+            $("#ip-policy-startup").prop('checked', row['startup']);
+            $("#ip-policy-id").val(row['id']);
+
+
+
+            
+            const modalTitle = ipModal.querySelector('.modal-title')
+            console.log(row);
+            modalTitle.textContent = `Edit ip policy ID:${row['id']}`
+        }
+    })
+
+
+    $("#ip-policy-form").on('submit', function (e) {
+        e.preventDefault();
+        var form = $(this);
+        var url = form.attr('action');
+        var id = $("#ip-policy-id").val();
+        var data = form.serialize();
+        console.log(data);
+        if (id != '') {
+            url = url + id + '/';
+            method = 'PUT';
+            console.log(url);
+        }
+        else {
+            method = 'POST';
+        }
+
+        $.ajax({
+            type: method,
+            url: url,
+            data: data,
+            success: function (data) {
+                console.log(data);
+                $('#ip-table').DataTable().ajax.reload()
+                $('#ip-modal').modal('hide');
+            },
+            error: function (err) {
+                console.log(err);
+                alert('Error: ' + err.responseText);
+            }
+        });
+    });
+
 
 
     // --------------------------------- Overview ---------------------------------
@@ -54,10 +302,7 @@ $(document).ready(function () {
         lengthMenu: [100, 200, 500],
         scrollY: "500px",
         scrollX: true,
-        scrollCollapse: false,
-        fixedColumns: {
-            left: 1,
-        }
+        scrollCollapse: false
 
     });
 
@@ -78,12 +323,13 @@ $(document).ready(function () {
             for (const interface of interfaces) {
                 const optionElement = document.createElement('option');
                 optionElement.textContent = interface;
-                $('#interface-selector').append(optionElement);
+                optionElement.value = interface;
+                $('#overview-interface-selector, #modal-interface-selector').append(optionElement);
             }
         });
 
     // Update the interface
-    $('#interface-selector').on('change', function () {
+    $('#overview-interface-selector').on('change', function () {
         // Clear the previous xhr object if it exists
         if (xhr) {
             console.log('aborting previous request');
@@ -132,7 +378,10 @@ $(document).ready(function () {
             url: 'program_policy/',
             dataSrc: ''
         },
-        order: [[0, 'desc']],
+        lengthMenu: [100, 200, 500],
+        scrollY: "500px",
+        scrollX: true,
+        scrollCollapse: false,
         "columnDefs": [{
             "defaultContent": "-",
             "targets": "_all",
@@ -149,8 +398,8 @@ $(document).ready(function () {
             { "data": "interface" },
             { "data": "enabled" },
             { "data": "startup" },
-            { "data": "description" },
             { "data": "created" },
+            { "data": "description" },
             {
                 "data": null,
                 "defaultContent": "<button>Edit</button>"
@@ -159,76 +408,30 @@ $(document).ready(function () {
                 "data": null,
                 "defaultContent": "<button>Delete</button>"
             }
-        ],
-        lengthMenu: [100, 200, 500],
-        scrollY: "500px",
-        scrollX: true,
-        scrollCollapse: false,
-        fixedColumns: {
-            left: 1,
-        },
-        initComplete: function () {
-            $(".delete-link").on('click', function (event) {
-                console.log("delete");
-                event.stopPropagation();
-                event.stopImmediatePropagation();
-                event.preventDefault();
-                row = $(this).parents('tr');
-                var deleteUrl = "/traffic_control/policy/" + $(this).data("id") + "/";
-                $.ajax({
-                    url: deleteUrl,
-                    type: "DELETE",
-                    success: function () {
-                        // Remove the row from the table
-                        console.log(programTable);
-                        $('#program-table').DataTable().ajax.reload()
-                        console.log("Done deleting object!");
-                    },
-                    error: function () {
-                        alert("Error deleting object!");
-                    }
-                });
-            });
-            $(".edit-link").on('click', function (event) {
-                console.log("edit");
-                event.stopPropagation();
-                event.stopImmediatePropagation();
-                event.preventDefault();
-                row = $(this).parents('tr');
-
-            });
-        }
-
-
+        ]
     })
-
-
-
-
-
-
-
-
-
 
 
     // --------------------------------- IP Policies ---------------------------------
 
-    var programTable = $('#ip-table').DataTable({
+    var ipTable = $('#ip-table').DataTable({
         ajax: {
             url: 'ip_policy/',
             dataSrc: ''
         },
+        lengthMenu: [100, 200, 500],
+        scrollY: "500px",
+        scrollX: true,
+        scrollCollapse: false,
 
-        order: [[0, 'desc']],
         "columnDefs": [{
             "defaultContent": "-",
             "targets": "_all",
+            "render": renderIpPolicy
         }],
         "columns": [
             { "data": "id" },
             { "data": "name" },
-            { "data": "direction" },
             { "data": "transport" },
             { "data": "ip_src" },
             { "data": "sport" },
@@ -237,6 +440,7 @@ $(document).ready(function () {
             { "data": "rate" },
             { "data": "burst" },
             { "data": "prio" },
+            { "data": "direction" },
             { "data": "interface" },
             { "data": "enabled" },
             { "data": "startup" },
@@ -251,15 +455,8 @@ $(document).ready(function () {
                 "defaultContent": "<button>Delete</button>"
             }
         ],
-        lengthMenu: [100, 200, 500],
-        scrollY: "500px",
-        scrollX: true,
-        scrollCollapse: false,
-        fixedColumns: {
-            left: 1,
-            right: 2,
-        }
     });
+
 
 
 
